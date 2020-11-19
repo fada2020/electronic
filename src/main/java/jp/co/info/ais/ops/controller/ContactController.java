8d1ea2d721e65249ef64bc50a62cc68df106ebb5
金鎭晩 <kimjm@ais-info.co.jp>
2020-11-19 13:51:39 +0900

    #李作業分反映

--- src/main/java/jp/co/info/ais/ops/controller/ContactController.java
+++ src/main/java/jp/co/info/ais/ops/controller/ContactController.java
@@ -1,40 +1,38 @@
 package jp.co.info.ais.ops.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/master")
-public class UserMasterController {
+@RequestMapping("/contact")
+public class ContactController {
 
 	//エラーを表すための宣言
-	private static final Logger logger = LogManager.getLogger(UserMasterController.class);
-
-	@Autowired
-	HttpSession session;
+	private static final Logger logger = LogManager.getLogger(ContactController.class);
 
 	/**
+	 * 設定一覧画面出力
 	 *
+	 * @param model
+	 * @return String　画面名
 	 */
-	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
-	public String EventList(Model model) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String SettingList(Model model) {
 		try {
 			//TODO:DBデータ取得及び加工処理追加
-			logger.debug("権限設定画面＝＝＝開始");
+			logger.debug("contact List Start");
+
 
 		}catch (Exception e) {
 			logger.debug(e.getMessage());
 		}
 		//戻り値
-		return "user_master";
-
+		return "contact_list";
 	}
+
 
 }
