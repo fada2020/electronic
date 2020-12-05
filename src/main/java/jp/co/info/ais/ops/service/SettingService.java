19bfc58e5e59ce93a074df41ef8a5977bf63ef42
ais191005 <hjlee@ais-info.co.jp>
2020-12-05 15:56:10 +0900

    #管理 #1884：機能修正

--- src/main/java/jp/co/info/ais/ops/service/SettingService.java
+++ src/main/java/jp/co/info/ais/ops/service/SettingService.java
@@ -15,20 +15,16 @@ import jp.co.info.ais.ops.mapper.postgre.SettingMapper;
 @Service
 public class SettingService {
 	private static final Logger logger = LogManager.getLogger(EventService.class);
-
 	@Autowired
-	SettingMapper settingMapper;
-
+	private SettingMapper settingMapper;
 	public List<Setting> settingList() throws Exception {
 		logger.debug("SettingListService START");
 		return settingMapper.settingList();
 	}
-
 	public int deleteShisetsuno(String shisetsuno)throws Exception  {
 		logger.debug("SettingdeleteService START");
 		return settingMapper.deleteShisetsuno(shisetsuno);
 	}
-
 	public int updateStatus(Setting setting) throws Exception  {
 		logger.debug("SettingUpdateStatusService START");
 		return settingMapper.updateStatus(setting);
