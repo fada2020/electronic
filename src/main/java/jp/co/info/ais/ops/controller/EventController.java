de9156db115fb6e030e3e9e7e944be6f5593e1eb
choisw <choisw@ais-info.co.jp>
2020-12-06 04:10:52 +0900

    イベントリストのcustomerとEventnameを(20)文字数に制限する

--- src/main/java/jp/co/info/ais/ops/controller/EventController.java
+++ src/main/java/jp/co/info/ais/ops/controller/EventController.java
@@ -102,6 +102,15 @@ public class EventController {
 			List<EventList> list = null;
 			list = eventService.selectEventList(paraMap);
 
+			for (EventList lengthcheck : list) {
+				if(lengthcheck.getCustomer().length()>20) {
+					lengthcheck.setCustomer(lengthcheck.getCustomer().substring(0,20) + "...");
+				}
+				if(lengthcheck.getEventname().length()>20) {
+					lengthcheck.setEventname(lengthcheck.getEventname().substring(0,20) + "...");
+				}
+			}
+
 			//戻る値設定
 			model.addAttribute("fromDate", fromDate);
 			model.addAttribute("toDate", toDate);
