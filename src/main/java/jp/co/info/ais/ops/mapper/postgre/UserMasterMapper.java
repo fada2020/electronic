842e102f62cbcaad67e3f0b9b496c7583e74ad9f
ais191005 <hjlee@ais-info.co.jp>
2020-12-06 14:34:32 +0900

    #管理 #1886：機能修正

--- src/main/java/jp/co/info/ais/ops/mapper/postgre/UserMasterMapper.java
+++ src/main/java/jp/co/info/ais/ops/mapper/postgre/UserMasterMapper.java
@@ -3,6 +3,7 @@ package jp.co.info.ais.ops.mapper.postgre;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.info.ais.ops.domain.UserMaster;
@@ -11,6 +12,7 @@ import jp.co.info.ais.ops.domain.UserMaster;
 @Mapper
 public interface UserMasterMapper {
 
-	public List<UserMaster> usermasterList()throws Exception;
+	public List<UserMaster> usermasterList(@Param("users")List<UserMaster> userMasterList)throws Exception;
+
 
 }
