d6d894f66ec014e99da69e54650cef6cfdaf7b32
金鎭晩 <kimjm@ais-info.co.jp>
2020-12-15 11:33:29 +0900

    tbl_jikahoinformuserlist SQL

---
+++ src/main/resources/sql/tbl_jikahoinformuserlist.sql
@@ -0,0 +1,10 @@
+-- public.tbl_jikahoinformuserlist definition
+
+-- Drop table
+
+-- DROP TABLE public.tbl_jikahoinformuserlist;
+
+CREATE TABLE public.tbl_jikahoinformuserlist (
+	customerno varchar(10) NULL,
+	loginuser varchar(16) NULL
+);
