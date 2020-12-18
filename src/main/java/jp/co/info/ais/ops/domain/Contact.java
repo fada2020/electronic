ddb0a3ecdcfa75818b8c495d40076bf325c8601c
ais191005 <hjlee@ais-info.co.jp>
2020-12-18 11:59:13 +0900

    #障害・不具合 #1939：対応

--- src/main/java/jp/co/info/ais/ops/domain/Contact.java
+++ src/main/java/jp/co/info/ais/ops/domain/Contact.java
@@ -1,6 +1,7 @@
 package jp.co.info.ais.ops.domain;
 
 public class Contact {
+	private String result;
 	private int rownum;//No
 	private int contactcd;//連絡先CD
 	private String contactname;//連絡先名称
@@ -8,57 +9,76 @@ public class Contact {
 	private String contactmailaddress;//連絡先メールアドレス
 	private int startcontactcd;//開始連絡先
 	private int endcontactcd;//終了連絡先
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
 	public int getRownum() {
 		return rownum;
 	}
+
 	public void setRownum(int rownum) {
 		this.rownum = rownum;
 	}
+
 	public int getContactcd() {
 		return contactcd;
 	}
+
 	public void setContactcd(int contactcd) {
 		this.contactcd = contactcd;
 	}
+
 	public String getContactname() {
 		return contactname;
 	}
+
 	public void setContactname(String contactname) {
 		this.contactname = contactname;
 	}
+
 	public String getContactphoneno() {
 		return contactphoneno;
 	}
+
 	public void setContactphoneno(String contactphoneno) {
 		this.contactphoneno = contactphoneno;
 	}
+
 	public String getContactmailaddress() {
 		return contactmailaddress;
 	}
+
 	public void setContactmailaddress(String contactmailaddress) {
 		this.contactmailaddress = contactmailaddress;
 	}
+
 	public int getStartcontactcd() {
 		return startcontactcd;
 	}
+
 	public void setStartcontactcd(int startcontactcd) {
 		this.startcontactcd = startcontactcd;
 	}
+
 	public int getEndcontactcd() {
 		return endcontactcd;
 	}
+
 	public void setEndcontactcd(int endcontactcd) {
 		this.endcontactcd = endcontactcd;
 	}
 
 	@Override
 	public String toString() {
-		return "Contact [rownum=" + rownum + ", contactcd=" + contactcd + ", contactname=" + contactname
-				+ ", contactphoneno=" + contactphoneno + ", contactmailaddress=" + contactmailaddress
+		return "Contact [result=" + result + ", rownum=" + rownum + ", contactcd=" + contactcd + ", contactname="
+				+ contactname + ", contactphoneno=" + contactphoneno + ", contactmailaddress=" + contactmailaddress
 				+ ", startcontactcd=" + startcontactcd + ", endcontactcd=" + endcontactcd + "]";
 	}
-
-
-
 
 }
